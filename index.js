821b152524ccec951bd411fd60acc4394cea9003
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-07-22 15:23:48 +0530

    chore: add shortcuts for help and home page

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ screen.key(['escape', 'q', 'C-c'], function(ch, key) {
   return process.exit(0);
 });
 
+
 var carousel = new contrib.carousel( [
   homePage, 
   fileSizesPage, 
@@ -20,5 +21,15 @@ var carousel = new contrib.carousel( [
     , interval: 0 //how often to switch views (set 0 to never swicth automatically)
     , controlKeys: true  //should right and left keyboard arrows control view rotation
   })
+
+// Show help page when ? is pressed
+screen.key(['?'], function(ch, key) {
+ carousel.end(); 
+});
+
+// Goto home when h key is pressed
+screen.key(['h'], function(ch, key) {
+  carousel.home();
+});
 
 module.exports = carousel;
